70e4784e671ce30048357e643a0cc20a8451a6f2
Adam K Chapman <adam.chap@hotmail.co.uk>
2016-12-03 13:47:15 +0000

    Sphere now Spins
    Created a new script 'Spin.CS' added it to the Sphere game object.

---
+++ SyncTest/Assets/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public float Speed;
+	
+	void Update () {
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * Speed, Space.Self);
+	}
+}
